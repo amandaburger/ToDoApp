cc64861a5e12e58ba918775c6fcad015565a7cc1
Amanda Burger <amandaburger01@gmail.com>
2020-05-19 13:16:05 -0400

    Initial Commit

---
+++ ToDoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoApp
+//
+//  Created by Amanda Burger on 5/19/20.
+//  Copyright © 2020 Amanda Burger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
