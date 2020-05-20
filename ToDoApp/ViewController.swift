2163c3893e37f9bd8ca02c7f128f26e8167d0e55
Amanda Burger <amandaburger01@gmail.com>
2020-05-20 16:04:15 -0400

    create to do list project with data that cannot be saved

--- ToDoApp/ViewController.swift
+++ ToDoApp/ViewController.swift
@@ -7,14 +7,44 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    var itemArr: [String] = []
 
+    
+    @IBOutlet var tablView: UITableView!
+    // View Did Load
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    //Add Pressed
+    @IBAction func addPressed(_ sender: Any) {
+        var tf = UITextField()
+        let alert = UIAlertController(title: "Add Item", message: "Add Item to the todo list?", preferredStyle: .alert)
+        let saveAction = UIAlertAction(title: "Save", style: .default) { (action) in
+            self.itemArr.append(tf.text!)
+            self.tableView.reloadData()
+        }
+        alert.addTextField { (alertTf) in
+            alertTf.placeholder = "Create new To Do List item"
+            tf = alertTf
+        }
+        alert.addAction(saveAction)
+        present(alert, animated: true)
+    }
+    
+    
+//  MARK: - Table View Data Source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArr.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = itemArr[indexPath.row]
+        return cell
+    }
+    
 }
-
