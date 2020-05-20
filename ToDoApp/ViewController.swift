d096078f3f47907d8c571d112719a5abd2063f84
Amanda Burger <amandaburger01@gmail.com>
2020-05-20 17:03:02 -0400

    implement coreData into code.... saves title and checkmark

--- ToDoApp/ViewController.swift
+++ ToDoApp/ViewController.swift
@@ -10,21 +10,35 @@ import UIKit
 import CoreData
 
 class ViewController: UITableViewController {
-    var itemArr: [String] = []
+    var itemArr: [NSManagedObject] = []
 
-    
+//MARK: View Did Load
     @IBOutlet var tablView: UITableView!
     // View Did Load
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
-    //Add Pressed
+    
+// MARK: View Will Appear
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let appDelegate = UIApplication.shared.delegate as? AppDelegate
+        let managedContext = appDelegate!.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Item")
+        do {
+          itemArr = try managedContext.fetch(fetchRequest)
+        } catch let error as NSError {
+          print("Could not fetch. \(error), \(error.userInfo)")
+        }
+    }
+    
+
+// MARK: Add Pressed
     @IBAction func addPressed(_ sender: Any) {
         var tf = UITextField()
         let alert = UIAlertController(title: "Add Item", message: "Add Item to the todo list?", preferredStyle: .alert)
         let saveAction = UIAlertAction(title: "Save", style: .default) { (action) in
-            self.itemArr.append(tf.text!)
+            self.save(title: tf.text!)
             self.tableView.reloadData()
         }
         alert.addTextField { (alertTf) in
@@ -43,8 +57,53 @@ class ViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = itemArr[indexPath.row]
+        let toDoItem = itemArr[indexPath.row]
+        let doneVal = itemArr[indexPath.row].value(forKey: "done") as? Bool
+        if doneVal == nil{
+            itemArr[indexPath.row].setValue(false, forKey: "done")
+        }
+        cell.textLabel?.text = toDoItem.value(forKey: "title") as? String
+        if doneVal == true{
+            cell.accessoryType = .checkmark
+        }else{
+            cell.accessoryType = .none
+        }
+        
         return cell
     }
     
+    
+// MARK: -SaveData Function
+    func save(title: String){
+        let appDelegate = UIApplication.shared.delegate as? AppDelegate
+        let managedContext = appDelegate!.persistentContainer.viewContext
+        let entity = NSEntityDescription.entity(forEntityName: "Item", in: managedContext)!
+        let item = NSManagedObject(entity: entity, insertInto: managedContext)
+        item.setValue(title, forKeyPath: "title")
+        item.setValue(false, forKey: "done")
+
+        do {
+            try managedContext.save()
+            itemArr.append(item)
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+        
+    }
+    
+// MARK: SelectRow
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        var doneVal = itemArr[indexPath.row].value(forKey: "done") as? Bool
+        if doneVal ==  nil{
+            doneVal = false
+        }
+        if doneVal == true{
+            itemArr[indexPath.row].setValue(false, forKey: "done")
+        }else{
+            itemArr[indexPath.row].setValue(true, forKey: "done")
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+        tableView.reloadData()
+    }
+        
 }
